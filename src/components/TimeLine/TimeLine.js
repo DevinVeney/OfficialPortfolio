4500cf2a7bf2b4b98065898030544db9cae683cb
Devin <devinveneyofficial@gmail.com>
2022-02-13 21:24:06 -0800

    syncing both branches

---
+++ src/components/TimeLine/TimeLine.js
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import { TimeLineData } from '../../constants/constants';
+
+const Timeline = () => {
+  const [setActiveItem] = useState(0);
+  const carouselRef = useRef();
+
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  };
+
+  const handleClick = (e, i) => {
+    e.preventDefault();
+
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
+      );
+
+      scroll(carouselRef.current, scrollLeft);
+    }
+  };
+
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length
+      );
+
+      setActiveItem(index);
+    }
+  };
+
+  // snap back to beginning of scroll when window is resized
+  // avoids a bug where content is covered up if coming from smaller screen
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    };
+
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <Section id="about">
+      <SectionTitle>My Journey</SectionTitle>
+      <SectionText>
+        In my journey to date, I have learned a great deal about development and
+        life in general.
+      </SectionText>
+    </Section>
+  );
+};
+
+export default Timeline;
